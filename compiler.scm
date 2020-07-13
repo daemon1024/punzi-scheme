b1b669f94dec6cb7703d43e4d4616daac603a400
daemon1024 <barun1024@gmail.com>
2020-07-14 00:57:56 +0530

    1.2 Immediate Constants

--- compiler.scm
+++ compiler.scm
@@ -1,11 +1,45 @@
 (load "tests/tests-driver.scm")
+(load "tests/tests-1.2-req.scm")
 (load "tests/tests-1.1-req.scm")
 
+(define fxshift 2)
+(define fxmask #x03)
+(define bool_f #x2F)
+(define bool_t #x6F)
+(define charmask  #x0F)
+(define chartag   #x0F)
+(define charshift 8)
+(define empty_list #x3F)
+(define wordsize 4) ; bytes
+
+(define fixnum-bits (- (* wordsize 8) fxshift))
+
+(define fxlower (- (expt 2 (- fixnum-bits 1))))
+
+(define fxupper (sub1 (expt 2 (- fixnum-bits 1))))
+
+(define (fixnum? x)
+(and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+(define (to-char x)
+  (logior (ash (char->integer x) charshift) chartag))
+
+(define (immediate? x)
+(or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
+(define (immediate-rep x)
+  (cond
+    [(fixnum? x) (ash x fxshift)]
+    [(boolean? x) (if x bool_t bool_f)]
+    [(char? x) (to-char x)]
+    [(null? x) empty_list]
+    [else #f]))
+
 (define (compile-program x)
-    (unless (integer? x) (error 'compile-program "not an integer"))
+    (unless (immediate? x) (error 'compile-program "not an immediate"))
     (emit " .text")
     (emit " .globl scheme_entry")
     (emit " .type scheme_entry, @function")
     (emit "scheme_entry:")
-    (emit " movl $~s, %eax" x)
+    (emit " movl $~s, %eax" (immediate-rep x))
     (emit " ret"))
--- startup.c
+++ startup.c
@@ -1,10 +1,70 @@
 #include <stdio.h>
 
-//declared to avoid warning `-Wimplicit-function-declaration`
-int scheme_entry();
+/* define all scheme constants */
+#define bool_f 0x2F
+#define bool_t 0x6F
+#define fx_mask 0x03
+#define fx_tag 0x00
+#define fx_shift 2
+#define char_mask 0x0F
+#define char_tag 0x0F
+#define char_shift 8
+#define empty_list 0x3F
 
+/* all scheme values are of type ptrs */
+typedef unsigned int ptr;
+
+//declared to avoid warning `-Wimplicit-function-declaration`
+ptr scheme_entry();
+
+static void print_ptr(ptr x)
+{
+    if ((x & fx_mask) == fx_tag)
+    {
+        printf("%d", ((int)x) >> fx_shift);
+    }
+    else if (x == bool_f)
+    {
+        printf("#f");
+    }
+    else if (x == bool_t)
+    {
+        printf("#t");
+    }
+    else if (x == empty_list)
+    {
+        printf("()");
+    }
+    else if ((x & char_mask) == char_tag)
+    {
+        char c = (char)(x >> char_shift);
+        switch (c)
+        {
+        case '\t':
+            printf("#\\tab");
+            break;
+        case '\n':
+            printf("#\\newline");
+            break;
+        case '\r':
+            printf("#\\return");
+            break;
+        case ' ':
+            printf("#\\space");
+            break;
+        default:
+            printf("#\\%c", c);
+            break;
+        }
+    }
+    else
+    {
+        printf("#<unknown 0x%08x>", x);
+    }
+    printf("\n");
+}
 int main(int argc, char **argv)
 {
-    printf("%d\n", scheme_entry());
+    print_ptr(scheme_entry());
     return 0;
 }
